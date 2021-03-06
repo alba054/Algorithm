af4d13b3d4477912d81fa02151bc7ac25731f885
alba054 <jralif02@gmail.com>
2021-03-06 23:08:03 +0800

    add dls algorithm

--- rat_in_a_maze/maze.js
+++ rat_in_a_maze/maze.js
@@ -60,6 +60,13 @@ class Node {
     }
 }
 
+class Node2 extends Node{
+    constructor(pos, par, action, depth) {
+        super(pos, par, action);
+        this.depth = depth;
+    }
+}
+
 // create maze in the browser
 class Maze {
     // construct Maze
@@ -136,8 +143,6 @@ class Maze {
     neighbors(pos) {
         const row = pos.row;
         const col = pos.col;
-        // console.log(row);
-        // console.log(col);
         const candidates = {
             up:[row - 1, col],
             down:[row + 1, col],
@@ -152,27 +157,13 @@ class Maze {
 
         const result = [];
         for(const candidate in candidates) {
-            // console.log(candidates[candidate][0]);
             const r = candidates[candidate][0];
             const c = candidates[candidate][1];
-            // console.log("row " + r);
-            // console.log("col " + c);
-            // console.log(this.encodedMaze[r][c]);
-            // try {
-                // if(this.encodedMaze[r][c] !== '#') {
-                //     result.push({action:candidate, pos:[r, c]});
-                // }
-                if((r >= 0 && r < this.encodedMaze.length) && (c >= 0 && c < this.encodedMaze[0].length)) {
-                    // console.log(`r:${r}, c:${c}`)
-                    if(this.encodedMaze[r][c] !== '#') {
-                        // console.log(r + " " +  c)
-                        result.push({action:candidate, pos:{row:r, col:c}});
-                    }
+            if((r >= 0 && r < this.encodedMaze.length) && (c >= 0 && c < this.encodedMaze[0].length)) {
+                if(this.encodedMaze[r][c] !== '#') {
+                    result.push({action:candidate, pos:{row:r, col:c}});
                 }
-            // }?
-            // catch(err) {
-            //     continue;
-            // }
+            }
         }
 
         return result;
@@ -257,17 +248,81 @@ class Maze {
         }
     }
 
-    drawSolution(algo) {
+    solveDLS(start, goal, algo, limit) {
+        // limit variable
+        const maxDepth = limit != -1 ? limit:-1;
+
+        // keep track of number of grids visited
+        this.numVisited = 0;
+
+        // initialize frontier (either stack or queue) with starting pos
+        const startNode = new Node2(start, null, null, maxDepth != -1 ? 0:-1);
+        const frontier = algo == "bfs" ? new Queue():new Stack();
+        frontier.add(startNode);
+
+        // initialize empty set for tracking visited grid
+        this.visited = new Set();
+
+        while(true) {
+            if(frontier.isEmpty()) {
+                return "no solution";
+            }
+
+            // choose any possible moves from one grid
+            let node = frontier.remove();
+            this.numVisited++;
+
+            // if grid is the goal
+            if(node.pos.row == goal.row && node.pos.col == goal.col) {
+                const actions = new Array();
+                const grids = new Array();
+                
+                while(node.par != null) {
+                    actions.push(node.action);
+                    grids.push(node.pos);
+                    node = node.par;
+                }
+
+                actions.reverse();
+                grids.reverse();
+                return {actions:actions, grids:grids};
+            }
+            
+            // mark grid as visited
+            this.visited.add(node.pos);
+            if(node.pos.row !== start.row && node.pos.col !== start.col) {
+                this.maze[node.pos.row][node.pos.col].style.backgroundColor = "pink";
+            }
+            
+            // skip if next node is beyond limit depth
+            if(maxDepth != -1) {
+                if(node.depth + 1 > limit) continue;
+            }
+
+            // add any possible moves from one grid to frontier
+            const neighbors = this.neighbors(node.pos);
+
+            for(let i = 0; i < neighbors.length; i++) {
+                const pos = neighbors[i].pos;
+                const action = neighbors[i].action;
+                if(!frontier.containState(pos) && !this.setContains(this.visited, pos)) {
+                    const newNode = new Node2(pos, node, action, node.depth+1);
+                    frontier.add(newNode);
+                }
+            }
+
+        }
+    }
+
+    drawSolution(algo, limit) {
         this.colorGrid();
         const start = {row:15, col:0};
         const goal = {row:8, col:13};
-        const solution = this.solve(start, goal, algo).grids;
-        // console.log(solution);
+        const solution = this.solveDLS(start, goal, algo, limit).grids;
         for(let i = 0; i < solution.length; i++) {
             const row = solution[i].row;
             const col = solution[i].col;
-            if((row == start.row && col == start.col) || (row == goal.row && goal.col)) continue;
-            // if(solution[i].row == goal.row && solution[i].col == goal.col) break;
+            // if((row == start.row && col == start.col) || (row == goal.row && goal.col)) continue;
             this.maze[solution[i].row][solution[i].col].style.backgroundColor = "yellow";
         }
     }
--- rat_in_a_maze/rat.js
+++ rat_in_a_maze/rat.js
@@ -23,15 +23,13 @@ A      ######################`;
 const maze = new Maze(strMaze, canvas);
 const bfsBtn = document.querySelector(".bfs")
 const dfsBtn = document.querySelector(".dfs")
+const dlsBtn = document.querySelector(".dls")
 
 
-bfsBtn.onclick = function() {
-    // maze.colorGrid();
-    maze.drawSolution("bfs");
-}
-dfsBtn.onclick = function() { 
-    // maze.colorGrid();
-    maze.drawSolution("dfs");
-};
+bfsBtn.onclick = function() { maze.drawSolution("bfs", -1); };
+dfsBtn.onclick = function() { maze.drawSolution("dfs", -1); };
+dlsBtn.onclick = function() { maze.drawSolution("dfs", 25); };
+
+
 // const solution = maze.solve(start, goal)
 // console.log(solution);
